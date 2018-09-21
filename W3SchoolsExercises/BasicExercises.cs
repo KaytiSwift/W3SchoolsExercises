d7e0ee8cce465fa62beff71caa6221d77dacb953
KaytiSwift <kaytiswift@yahoo.com>
2018-09-20 21:34:09 -0400

    added additional exercises

--- W3SchoolsExercises/BasicExercises.cs
+++ W3SchoolsExercises/BasicExercises.cs
@@ -12,45 +12,87 @@ namespace W3SchoolsExercises
         {
             //Basic Exercises
             //1.
-            Console.WriteLine("Hello");
-            Console.WriteLine("Kayti");
+            //Console.WriteLine("Hello");
+            //Console.WriteLine("Kayti");
 
             //2.
-            Console.WriteLine(2 + 2);
+            //Console.WriteLine(2 + 2);
 
             //3.
-            Console.WriteLine(4 / 2);
+            //Console.WriteLine(4 / 2);
 
             //4.
-            Console.WriteLine(-1 + 4 * 6);
-            Console.WriteLine((35 + 5) % 7);
-            Console.WriteLine(14 + -4 * 6 / 11);
-            Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
+            //Console.WriteLine(-1 + 4 * 6);
+            //Console.WriteLine((35 + 5) % 7);
+            //Console.WriteLine(14 + -4 * 6 / 11);
+            //Console.WriteLine(2 + 15 / 6 * 1 - 7 % 2);
 
             //5.
-            int number1, number2, temp;
-            Console.WriteLine("Input the first number:");
-            number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input the second number:");
-            number2 = int.Parse(Console.ReadLine());
-            temp = number1;
-            number1 = number2;
-            number2 = temp;
-            Console.WriteLine("After swapping :");
-            Console.WriteLine("First Number : " + number1);
-            Console.WriteLine("Second Number : " + temp);
+            //int number1, number2, temp;
+            //Console.WriteLine("Input the first number:");
+            //number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Input the second number:");
+            //number2 = int.Parse(Console.ReadLine());
+            //temp = number1;
+            //number1 = number2;
+            //number2 = temp;
+            //Console.WriteLine("After swapping :");
+            //Console.WriteLine("First Number : " + number1);
+            //Console.WriteLine("Second Number : " + temp);
 
             //6. 
-            Console.WriteLine("Input the first number to multiply:");
-            number1 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input the second number to multiply:");
-            number2 = int.Parse(Console.ReadLine());
-            Console.WriteLine("Input the third number to multiply:");
-            int number3 = int.Parse(Console.ReadLine());
-            int result = number1 * number2 * number3;
-            Console.WriteLine("{0} X {1} X {2} = {3}", number1, number2, number3, result);
+            //Console.WriteLine("Input the first number to multiply:");
+            //number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Input the second number to multiply:");
+            //number2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Input the third number to multiply:");
+            //int number3 = int.Parse(Console.ReadLine());
+            //int result = number1 * number2 * number3;
+            //Console.WriteLine("{0} X {1} X {2} = {3}", number1, number2, number3, result);
 
             //7. 
+            //Console.WriteLine("Input the first number:");
+            //int number1 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Input the second number:");
+            //int number2 = int.Parse(Console.ReadLine());
+            //int result1 = number1 + number2;
+            //int result2 = number1 - number2;
+            //int result3 = number1 * number2;
+            //int result4 = number1 / number2;
+            //int result5 = number1 % number2;
+            //Console.WriteLine("{0} + {1} = {2}", number1, number2, result1);
+            //Console.WriteLine("{0} - {1} = {2}", number1, number2, result2);
+            //Console.WriteLine("{0} X {1} = {2}", number1, number2, result3);
+            //Console.WriteLine("{0} / {1} = {2}", number1, number2, result4);
+            //Console.WriteLine("{0} mod {1} = {2}", number1, number2, result5);
+
+            //8.
+            //Console.WriteLine("Enter the number: ");
+            //int x = int.Parse(Console.ReadLine());
+            //int result;
+            //result = x * 0;
+            //Console.WriteLine("{0} X {1} = {2}", x, 0, result);
+            //result = x * 1;
+            //Console.WriteLine("{0} X {1} = {2}", x, 1, result);
+            //result = x * 2;
+            //Console.WriteLine("{0} X {1} = {2}", x, 2, result);
+            //result = x * 3;
+            //Console.WriteLine("{0} X {1} = {2}", x, 3, result);
+            //result = x * 4;
+            //Console.WriteLine("{0} X {1} = {2}", x, 4, result);
+            //result = x * 5;
+            //Console.WriteLine("{0} X {1} = {2}", x, 5, result);
+            //result = x * 6;
+            //Console.WriteLine("{0} X {1} = {2}", x, 6, result);
+            //result = x * 7;
+            //Console.WriteLine("{0} X {1} = {2}", x, 7, result);
+            //result = x * 8;
+            //Console.WriteLine("{0} X {1} = {2}", x, 8, result);
+            //result = x * 9;
+            //Console.WriteLine("{0} X {1} = {2}", x, 9, result);
+            //result = x * 10;
+            //Console.WriteLine("{0} X {1} = {2}", x, 10, result);
+            
 
         }
     }
